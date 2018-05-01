687456f6aa9327dfaedb8cb53dc6ce694a8c5db2
ckghosted <ckghosted@gmail.com>
2018-05-02 02:51:20 +0800

    remove '\r' from hw3.sh and hw3_best.sh

--- hw3/hw3.sh
+++ hw3/hw3.sh
@@ -1,3 +1,3 @@
 wget -O model.zip "https://www.dropbox.com/s/gzncos15uoyth5s/models_VGG16_FCN32s.zip?dl=0"
-unzip model.zip
-python3 inference.py --testing_images "$1" --output_images "$2" --fcn_stride 32 --gen_from models/
+unzip model.zip -d VGG16_FCN32s
+python3 inference.py --testing_images "$1" --output_images "$2" --fcn_stride 32 --gen_from VGG16_FCN32s/models
--- hw3/hw3_best.sh
+++ hw3/hw3_best.sh
@@ -1,3 +1,3 @@
 wget -O model.zip "https://www.dropbox.com/s/ynnjuj2mvku8605/models_VGG16_FCN8s.zip?dl=0"
-unzip model.zip
-python3 inference.py --testing_images "$1" --output_images "$2" --fcn_stride 8 --gen_from models_VGG16_FCN8s
+unzip model.zip -d VGG16_FCN8s
+python3 inference.py --testing_images "$1" --output_images "$2" --fcn_stride 8 --gen_from VGG16_FCN8s/models
