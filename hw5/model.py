41c01235d40577adccffa5a86ded793b0497596f
ckghosted <ckghosted@gmail.com>
2018-06-08 15:39:06 +0800

    use dynamic_rnn

--- hw5/model.py
+++ hw5/model.py
@@ -2,6 +2,8 @@ import os, re, time, glob
 import numpy as np
 import tensorflow as tf
 from tensorflow.contrib.layers import batch_norm
+from tensorflow.contrib import rnn
+
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
@@ -282,7 +284,8 @@ class DNN(object):
         return [loss_train, loss_valid, acc_train, acc_valid]
     
     def inference(self,
-                  feature_path='/data/put_data/cclin/ntu/dlcv2018/hw5/cnn_features_valid.npy',
+                  #feature_path='/data/put_data/cclin/ntu/dlcv2018/hw5/cnn_features_valid.npy',
+                  cnn_features,
                   label_file='/data/put_data/cclin/ntu/dlcv2018/hw5/HW5_data/TrimmedVideos/label/gt_valid.csv',
                   gen_from=None,
                   gen_from_ckpt=None,
@@ -305,7 +308,8 @@ class DNN(object):
             print(" [*] Load SUCCESS")
             
             ## load features and take average over all frames of a video
-            features = [np.mean(f, axis=0) for f in np.load(feature_path)]
+            #features = [np.mean(f, axis=0) for f in np.load(feature_path)]
+            features = [np.mean(f, axis=0) for f in cnn_features]
             nBatches = int(np.ceil(len(features) / bsize))
             ## load labels (and make them one-hot vectors)
             video_list = getVideoList(label_file)
@@ -331,7 +335,7 @@ class DNN(object):
             print('valid loss: %f, valid accuracy: %f' % \
                   (np.mean(loss_batch), np.mean(acc_batch)))
             with open(os.path.join(out_path, 'p1_valid.txt'), 'w') as f:
-                for y in y_pred:
+                for y in y_pred_list:
                     f.write(str(y)+'\n')
     
     def load(self, init_from, init_from_ckpt=None):
@@ -369,7 +373,9 @@ class LSTM(object):
         self.epsilon = epsilon
     
     ## Build the discriminative model
-    def build_model(self, feature_dim=40960):
+    def build_model(self,
+                    use_static_rnn=True,
+                    feature_dim=40960):
         self.features = tf.placeholder(tf.float32, shape=[None, self.max_seq_len, feature_dim], name='features')
         self.labels = tf.placeholder(tf.int32, shape=[None, self.n_class], name='labels')
         self.seqlen = tf.placeholder(tf.int32, [None])
@@ -378,38 +384,47 @@ class LSTM(object):
         
         ## batch normalization
         self.bn1 = batch_norm(epsilon=self.epsilon, momentum=self.bnDecay, name='bn1')
-        self.bn2 = batch_norm(epsilon=self.epsilon, momentum=self.bnDecay, name='bn2')
+        #self.bn2 = batch_norm(epsilon=self.epsilon, momentum=self.bnDecay, name='bn2')
         
-        ## dynamic_rnn()
-        rnn_layers = [rnn.BasicLSTMCell(self.h_dim, forget_bias=1.0),
-                      rnn.BasicLSTMCell(self.h_dim, forget_bias=1.0)]
-        multi_rnn_cell = rnn.MultiRNNCell(rnn_layers)
-        # 'outputs' is a tensor of shape [batch_size, max_time, 256]
-        # 'state' is a N-tuple where N is the number of LSTMCells containing a
-        # tf.contrib.rnn.LSTMStateTuple for each cell
-        #outputs, state = tf.nn.dynamic_rnn(cell=multi_rnn_cell,
-        #                                   inputs=self.features,
-        #                                   dtype=tf.float32)
-
-        ## Unstack to get a list of 'max_seq_len' tensors of shape (batch_size, n_input)
-        x = tf.unstack(self.features, self.max_seq_len, 1)
-        ## Define a lstm cell with tensorflow
-        #lstm_cell = rnn.BasicLSTMCell(self.h_dim, forget_bias=1.0)
-        ## Get lstm cell output (providing 'sequence_length' will perform dynamic calculation)
-        outputs, states = rnn.static_rnn(multi_rnn_cell, x, dtype=tf.float32, sequence_length=self.seqlen)
-        ## Hack to build the indexing and retrieve the right output
-        outputs = tf.stack(outputs) ### np.array with shape (self.max_seq_len, batch_size, feature_dim)
-        outputs = tf.transpose(outputs, [1, 0, 2]) ### np.array with shape (batch_size, self.max_seq_len, feature_dim)
+        ## Approach 1: use static_rnn() with sequence_length to perform dynamic calculation
+        if use_static_rnn:
+            ### Unstack to get a list of 'max_seq_len' tensors of shape (batch_size, n_input)
+            x = tf.unstack(self.features, self.max_seq_len, 1)
+            ### (1) single layer:
+            #lstm_cell = rnn.BasicLSTMCell(self.h_dim, forget_bias=1.0)
+            #outputs, states = rnn.static_rnn(lstm_cell, x, dtype=tf.float32, sequence_length=self.seqlen)
+            ### (2) multiple layers:
+            rnn_layers = [rnn.BasicLSTMCell(self.h_dim, forget_bias=1.0),
+                          rnn.BasicLSTMCell(self.h_dim, forget_bias=1.0)]
+            multi_rnn_cell = rnn.MultiRNNCell(rnn_layers)
+            outputs, states = rnn.static_rnn(multi_rnn_cell, x, dtype=tf.float32, sequence_length=self.seqlen)
+            ### In this case, 'outputs' will be a list of length self.max_seq_len,
+            ### with each element being a tensor of shape (batch_size, self.h_dim).
+            outputs = tf.stack(outputs) ### shape: (self.max_seq_len, batch_size, self.h_dim)
+            outputs = tf.transpose(outputs, [1, 0, 2]) ### shape: (batch_size, self.max_seq_len, self.h_dim)
+        ## Approach 2: dynamic_rnn()
+        else:
+            ### (1) single layer:
+            #lstm_cell = rnn.BasicLSTMCell(self.h_dim, forget_bias=1.0)
+            #outputs, states = tf.nn.dynamic_rnn(lstm_cell, self.features, dtype=tf.float32, sequence_length=self.seqlen)
+            ### (2) multiple layers:
+            rnn_layers = [rnn.BasicLSTMCell(self.h_dim, forget_bias=1.0),
+                          rnn.BasicLSTMCell(self.h_dim, forget_bias=1.0)]
+            multi_rnn_cell = rnn.MultiRNNCell(rnn_layers)
+            outputs, states = tf.nn.dynamic_rnn(multi_rnn_cell, self.features, dtype=tf.float32, sequence_length=self.seqlen)
+            ### In this case, 'outputs' will be a tensor of shape (batch_size, self.max_seq_len, self.h_dim).
+        
+        ## Hack to build the indexing and retrieve the right output:
         batch_size = tf.shape(outputs)[0]
-        ## Start indices for each sample
+        ## start indices for each sample
         index = tf.range(0, batch_size) * self.max_seq_len + (self.seqlen - 1)
-        ## Indexing
+        ## indexing
         outputs = tf.gather(tf.reshape(outputs, [-1, self.h_dim]), index)
         
         ## take the last output of the rnn inner loop
-        h1 = linear(outputs, 128, 'h1')
+        h1 = linear(outputs, self.h_dim/2, 'h1')
         h1 = tf.nn.relu(self.bn1(h1, train=self.bn_train))
-        #h2 = linear(h1, 512, 'h2')
+        #h2 = linear(h1, 256, 'h2')
         #h2 = tf.nn.relu(self.bn2(h2, train=self.bn_train))
         self.logits = linear(h1, self.n_class, 'logits')
         
@@ -418,6 +433,7 @@ class LSTM(object):
         
         ## training operation
         self.t_vars = tf.trainable_variables()
+        print(self.t_vars)
         self.train_op = tf.train.AdamOptimizer(learning_rate=self.learning_rate, beta1=0.5).minimize(self.loss, var_list=self.t_vars)
         
         ## Create model saver (keep the best 3 checkpoint)
@@ -430,7 +446,8 @@ class LSTM(object):
               label_file_valid='/data/put_data/cclin/ntu/dlcv2018/hw5/HW5_data/TrimmedVideos/label/gt_valid.csv',
               bsize=32,
               learning_rate=2e-4,
-              num_epoch=200):
+              num_epoch=200,
+              patience=10):
         ## create a dedicated folder for this model
         if os.path.exists(os.path.join(self.result_path, self.model_name)):
             print('WARNING: the folder "{}" already exists!'.format(os.path.join(self.result_path, self.model_name)))
@@ -441,6 +458,12 @@ class LSTM(object):
         ## load training features
         features_train = np.load(feature_path_train)
         nBatches = int(np.ceil(len(features_train) / bsize))
+        ## down-sampling to make sure all videos have number of frames <= self.max_seq_len
+        features_train_new = []
+        for v_idx in range(len(features_train)):
+            n_frames = features_train[v_idx].shape[0]
+            step = (n_frames - 1) // self.max_seq_len + 1
+            features_train_new.append(features_train[v_idx][np.arange(0,n_frames,step),:])
         ## load training labels (and make them one-hot vectors)
         video_list_train = getVideoList(label_file_train)
         labels_train = [int(s) for s in video_list_train['Action_labels']]
@@ -448,6 +471,12 @@ class LSTM(object):
         ## load validation features
         features_valid = np.load(feature_path_valid)
         nBatches_valid = int(np.ceil(len(features_valid) / bsize))
+        ## down-sampling to make sure all videos have number of frames <= self.max_seq_len
+        features_valid_new = []
+        for v_idx in range(len(features_valid)):
+            n_frames = features_valid[v_idx].shape[0]
+            step = (n_frames - 1) // self.max_seq_len + 1
+            features_valid_new.append(features_valid[v_idx][np.arange(0,n_frames,step),:])
         ## load validation labels (and make them one-hot vectors)
         video_list_valid = getVideoList(label_file_valid)
         labels_valid = [int(s) for s in video_list_valid['Action_labels']]
@@ -462,6 +491,11 @@ class LSTM(object):
         loss_valid = []
         acc_train = []
         acc_valid = []
+        best_loss = 0
+        stopping_step = 0
+        
+        #print('start training')
+        
         for epoch in range(1, (num_epoch+1)):
             loss_train_batch = []
             loss_valid_batch = []
@@ -469,7 +503,7 @@ class LSTM(object):
             acc_valid_batch = []
             for idx in range(nBatches-1):
                 #print(idx)
-                batch_features = features_train[idx*bsize:(idx+1)*bsize]
+                batch_features = features_train_new[idx*bsize:(idx+1)*bsize]
                 ### padding 0
                 batch_features_pad = np.array([np.vstack((f, np.zeros((self.max_seq_len - f.shape[0], f.shape[1])))) \
                                                if f.shape[0] < self.max_seq_len \
@@ -493,7 +527,7 @@ class LSTM(object):
             ### compute validation loss
             for idx in range(nBatches_valid):
                 #print(idx)
-                batch_features = features_valid[idx*bsize:(idx+1)*bsize]
+                batch_features = features_valid_new[idx*bsize:(idx+1)*bsize]
                 ### padding 0
                 batch_features_pad = np.array([np.vstack((f, np.zeros((self.max_seq_len - f.shape[0], f.shape[1])))) \
                                                if f.shape[0] < self.max_seq_len \
@@ -520,7 +554,93 @@ class LSTM(object):
             acc_valid.append(np.mean(acc_valid_batch))
             print('Epoch: %d, train loss: %f, valid loss: %f, train accuracy: %f, valid accuracy: %f' % \
                   (epoch, np.mean(loss_train_batch), np.mean(loss_valid_batch), np.mean(acc_train_batch), np.mean(acc_valid_batch)))
+            
+            ### save model if improvement, stop if reach patience
+            current_loss = np.mean(loss_valid_batch)
+            if epoch == 1:
+                best_loss = current_loss
+            else:
+                if current_loss < best_loss:
+                    best_loss = current_loss
+                    self.saver.save(self.sess,
+                                    os.path.join(self.result_path, self.model_name, 'models', self.model_name + '.model'),
+                                    global_step=epoch)
+                    stopping_step = 0
+                else:
+                    stopping_step += 1
+                print('stopping_step = %d' % stopping_step)
+                if stopping_step >= patience:
+                    print('stopping_step >= patience (%d), stop training' % patience)
+                    break
         return [loss_train, loss_valid, acc_train, acc_valid]
+    
+    def inference(self,
+                  #feature_path='/data/put_data/cclin/ntu/dlcv2018/hw5/cnn_features_valid.npy',
+                  cnn_features,
+                  label_file='/data/put_data/cclin/ntu/dlcv2018/hw5/HW5_data/TrimmedVideos/label/gt_valid.csv',
+                  gen_from=None,
+                  gen_from_ckpt=None,
+                  out_path=None,
+                  bsize=64):
+        ## create output folder
+        if gen_from is None:
+            gen_from = os.path.join(self.result_path, self.model_name, 'models')
+        if out_path is not None:
+            if os.path.exists(out_path):
+                print('WARNING: the output path "{}" already exists!'.format(out_path))
+            else:
+                os.makedirs(out_path)
+        else:
+            out_path = os.path.join(self.result_path, self.model_name)
+        
+        ## load previous model if possible
+        could_load, checkpoint_counter = self.load(gen_from, gen_from_ckpt)
+        if could_load:
+            print(" [*] Load SUCCESS")
+            
+            ## load features and take average over all frames of a video
+            #features = np.load(feature_path)
+            features = cnn_features
+            nBatches = int(np.ceil(len(features) / bsize))
+            ## down-sampling to make sure all videos have number of frames <= self.max_seq_len
+            features_new = []
+            for v_idx in range(len(features)):
+                n_frames = features[v_idx].shape[0]
+                step = (n_frames - 1) // self.max_seq_len + 1
+                features_new.append(features[v_idx][np.arange(0,n_frames,step),:])
+            ## load labels (and make them one-hot vectors)
+            video_list = getVideoList(label_file)
+            labels = [int(s) for s in video_list['Action_labels']]
+            labels_vec = np.eye(self.n_class)[labels]
+            
+            ### p2_result.txt: make prediction
+            loss_batch = []
+            acc_batch = []
+            y_pred_list = []
+            for idx in range(nBatches):
+                batch_features = features_new[idx*bsize:(idx+1)*bsize]
+                ### padding 0
+                batch_features_pad = np.array([np.vstack((f, np.zeros((self.max_seq_len - f.shape[0], f.shape[1])))) \
+                                               if f.shape[0] < self.max_seq_len \
+                                               else f[0:self.max_seq_len] \
+                                               for f in batch_features])
+                batch_labels = labels_vec[idx*bsize:(idx+1)*bsize]
+                batch_seqlen = np.array([np.min((f.shape[0], self.max_seq_len)) for f in batch_features], dtype=np.int32)
+                loss, logits = self.sess.run([self.loss, self.logits],
+                                             feed_dict={self.features: batch_features_pad,
+                                                        self.labels: batch_labels,
+                                                        self.seqlen: batch_seqlen,
+                                                        self.bn_train: False})
+                loss_batch.append(loss)
+                y_true = np.argmax(batch_labels, axis=1)
+                y_pred = np.argmax(logits, axis=1)
+                acc_batch.append(accuracy_score(y_true, y_pred))
+                y_pred_list.extend(y_pred)
+            print('valid loss: %f, valid accuracy: %f' % \
+                  (np.mean(loss_batch), np.mean(acc_batch)))
+            with open(os.path.join(out_path, 'p2_result.txt'), 'w') as f:
+                for y in y_pred_list:
+                    f.write(str(y)+'\n')
     
     def load(self, init_from, init_from_ckpt=None):
         ckpt = tf.train.get_checkpoint_state(init_from)
@@ -536,7 +656,6 @@ class LSTM(object):
         else:
             print(" [*] Failed to find a checkpoint")
             return False, 0
-
 
 
 
